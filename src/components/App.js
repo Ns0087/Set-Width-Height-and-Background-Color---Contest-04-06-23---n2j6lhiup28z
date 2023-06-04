c26f40cc6e73f46208cd8e9e20124c7f95e3b076
Nitin Saraswat <117112997+Ns0087@users.noreply.github.com>
2023-06-04 12:51:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,33 @@ const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [boxes, setBoxes] = useState([]);
+  
+  const handleWidth = (e) => {
+    setWidth(e.target.value);
+  }
+  
+  const handleHeight = (e) => {
+    setWidth(e.target.value);
+  }
 
+  const handleBackgroundColor = (e) => {
+    setWidth(e.target.value);
+  }
+  
+  const handleCreateBox = () => {
+    const newBox = {
+      width,
+      height,
+      backgroundColor,
+    };
+    
+    setBoxes([...boxes, newBox]);
+    setWidth('');
+    setHeight('');
+    setBackgroundColor('');
+  }
+  
   return (
     <div id="main">
       <h1>Box Creator</h1>
@@ -14,6 +40,8 @@ const App = () => {
         <input
           id="width"
           type="number"
+          value={width}
+          onChange={handleWidth}
         />
       </div>
       <div>
@@ -21,6 +49,8 @@ const App = () => {
         <input
           id="height"
           type="number"
+          value={height}
+          onChange={handleHeight}
         />
       </div>
       <div>
@@ -28,10 +58,16 @@ const App = () => {
         <input
           id="backgroundColor"
           type="text"
+          value={backgroundColor}
+          onChange={handleBackgroundColor}
         />
       </div>
-      <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <button className='create-btn' onClick={handleCreateBox}>Create Box</button>
+      <div id="box-container">
+        {boxes.Map((box, index) => (
+          <div key={index} style={{width: box.width, height: box.height, backgroundColor: box.backgroundColor, margin: '10px',}}></div>
+        ))}
+        </div>
     </div>
   );
 };
